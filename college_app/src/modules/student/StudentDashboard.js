a9254a133a9a04ce47c106ddf6e9bd0433543cc0
kasinathan28 <way2kasinathb35@gmail.com>
2024-04-02 09:40:23 +0530

    Last

--- college_app/src/modules/student/StudentDashboard.js
+++ college_app/src/modules/student/StudentDashboard.js
@@ -1,10 +1,10 @@
-import { useNavigate } from 'react-router-dom';
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import "./StudentDashboard.css";
 import Navbar from "../../components/navbar/Navbar";
 import StudentProfile from "./components/studentprofile/StudentProfile";
 import Certificates from "./components/certificates/Certificates";
+import welcomeImage from "../../assets/student-img.png";
 
 function StudentDashboard() {
   const { id } = useParams();
@@ -14,10 +14,8 @@ function StudentDashboard() {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    // Implement your logout logic here, such as clearing local storage, etc.
     console.log("Logging out...");
-    // For demonstration purposes, let's navigate to the login page
-    navigate('/'); // Update the route to your actual login route
+    navigate("/"); // Navigate to the login page
   };
 
   const renderContent = () => {
@@ -26,9 +24,24 @@ function StudentDashboard() {
         return <StudentProfile id={id} />;
       case "certificates":
         return <Certificates id={id} />;
-
       default:
-        return <div>Select an option to view content</div>;
+        // Display the welcome image by default
+        return (
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              margin: "auto",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection:"column"
+            }}
+          >
+
+            <img src={welcomeImage} alt="Welcome" />
+            <h2>Student Dashboard</h2>
+          </div>
+        );
     }
   };
 
@@ -49,7 +62,9 @@ function StudentDashboard() {
               </button>
             </li>
             <li>
-              <button onClick={handleLogout}>Logout</button>
+              <button onClick={handleLogout} id="logout">
+                Logout
+              </button>
             </li>
           </ul>
         </div>
--- college_app/src/modules/teacher/TeacherDashboard.js
+++ college_app/src/modules/teacher/TeacherDashboard.js
@@ -1,45 +1,40 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import './TeacherDashboard.css';
-import Navbar from '../../components/navbar/Navbar';
-import TeacherProfile from './components/TeacherProfile/TeacherProfile';
-import Students from './components/Students/Students';
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import "./TeacherDashboard.css";
+import Navbar from "../../components/navbar/Navbar";
+import TeacherProfile from "./components/TeacherProfile/TeacherProfile";
+import Students from "./components/Students/Students";
 import thrgif from "../../assets/teacherBanner.png";
 
 function TeacherDashboard() {
   const { id } = useParams();
   const [selectedOption, setSelectedOption] = useState(null);
 
-
   const navigate = useNavigate();
 
-
   const handleLogout = () => {
-    
-    console.log("Logging out...");
-    
-    navigate('/'); 
+    navigate("/");
   };
 
-
-
-  
   const renderContent = () => {
     switch (selectedOption) {
-      case 'profile':
+      case "profile":
         return <TeacherProfile id={id} />;
-      case 'students':
-        return <Students/> ;
-      case 'teachers':
-        
-    
+      case "students":
+        return <Students />;
+      case "teachers":
+
       default:
-        return <div className='admigifcontainer'>
-        <div className='admincot'>
-        <h1>Make the changes you want. It may affect all the faculties.</h1>
-        </div>
-        <img  className="admgif" src={thrgif} />
-      </div>;
+        return (
+          <div className="admigifcontainer">
+            <div className="admincot">
+              <h1>
+                Make the changes you want. It may affect all the faculties.
+              </h1>
+            </div>
+            <img className="admgif" src={thrgif} />
+          </div>
+        );
     }
   };
 
@@ -50,16 +45,23 @@ function TeacherDashboard() {
         <div className="sidebar">
           <ul>
             <li>
-              <button onClick={() => setSelectedOption('profile')}>MY PROFILE</button>
+              <button onClick={() => setSelectedOption("profile")}>
+                MY PROFILE
+              </button>
             </li>
             <li>
-              <button onClick={() => setSelectedOption('students')}>STUDENTS</button>
-              </li>
+              <button onClick={() => setSelectedOption("students")}>
+                STUDENTS
+              </button>
+            </li>
+            <li>
+              <button onClick={handleLogout} id="logout">
+                Logout
+              </button>
+            </li>
           </ul>
         </div>
-        <div className="content">
-          {renderContent()}
-        </div>
+        <div className="content">{renderContent()}</div>
       </div>
     </div>
   );
